1ef0d3ae6ee3692ec4c4ac7a58e2cbe51311ad94
rkarmaka <rkarmaka98@gmail.com>
2024-10-06 12:56:24 +0100

    adding 2 filters and option to select them by using buttons

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 **Linux**
 - to create dependency file use `pip freeze > requirements.txt`
 - to install environment run  `chmod +x setup_env.sh` then `./setup_env.sh`
+
 **Windows**
 - Double-click the `setup_env.bat` file or run it from the command line
--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)
 
-def generate_frames():
+def generate_frames(filter_type):
     while True:
         success, frame = camera.read()
         if not success:
             break
         else:
-            # Apply a simple filter (e.g., grayscale)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, buffer = cv2.imencode('.jpg', gray)
+            # Adding other filters
+            if filter_type == 'gray':
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            elif filter_type == 'canny':
+                frame = cv2.Canny(frame, 100, 200)
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -24,7 +28,8 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    filter_type = request.args.get('filter', 'none')
+    return Response(generate_frames(filter_type), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
